dffb13604f83893f533b95a0970f51af7d3d4a61
Leru Xie <1486835097@qq.com>
2021-06-09 10:11:47 +0800

    refine: 修改一些

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import './App.css';
-import Post from './pages/post';
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import RouteAuth from "./router/RouteAuth";
+import "./App.css";
 
 function App() {
   return (
-    <Post />
+    <div className="App">
+      <Router>
+        <Switch>
+          <RouteAuth />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
