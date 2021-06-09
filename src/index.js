0b2beb83e97603aba956349633c7faa02949f61f
Leru Xie <1486835097@qq.com>
2021-06-09 10:15:21 +0800

    feat: add css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import './index.css'
 import "@/assets/style/theme.less";
 import store from "./modules";
 import { Provider } from "react-redux";
