6bb140b509d9904ea656c5c3852945ad39259841
Leru Xie <1486835097@qq.com>
2021-06-10 14:28:38 +0800

    feat: navbar 和 高度坍塌问题

--- src/layouts/layout/index.jsx
+++ src/layouts/layout/index.jsx
@@ -1,21 +1,23 @@
-import { Layout as ALayout } from 'antd';
-import { useHistory } from 'react-router-dom';
-import Sidebar from '../sidebar/index';
+import { Layout as ALayout } from "antd";
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import Sidebar from "../sidebar/index";
 
 const { Content } = ALayout;
 
 const Layout = ({ children }) => {
-    const history = useHistory();
-    const { pathname } = history.location;
-
-    return (
-        <ALayout>
-            { pathname.indexOf("login") === -1 ? <Sidebar /> : ""}
-            <Content>
-                {children}
-            </Content>
-        </ALayout>
-    );
+  const history = useHistory();
+  const [showSidebar, setShowSideBar] = useState(false);
+  useEffect(() => {
+    console.log(history.location.pathname)
+    setShowSideBar(history.location.pathname.indexOf("login") === -1);
+  }, [history]);
+  return (
+    <ALayout>
+      {showSidebar && <Sidebar />}
+      <Content>{children}</Content>
+    </ALayout>
+  );
 };
 
 export default Layout;
