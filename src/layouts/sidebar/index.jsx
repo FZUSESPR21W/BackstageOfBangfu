ad43283b2c802548dd419050a2612c5d89c08d4f
lilith <1131155106@qq.com>
2021-06-12 21:37:16 +0800

    feat:完成导航栏路由的跳转(除用户管理)

--- src/layouts/sidebar/index.jsx
+++ src/layouts/sidebar/index.jsx
@@ -5,32 +5,48 @@ import {
     UserOutlined,
     LogoutOutlined,
 } from '@ant-design/icons';
+import { useHistory } from 'react-router-dom';
 import "./index.less";
 
 const { SubMenu } = Menu;
 
 
 const Sidebar = (props) => {
+    const history = useHistory();
+
+    const handleItemClick = (item) => {
+        const { key } = item;
+        if (key === "login") {
+            localStorage.clear();
+        }
+
+        history.replace(`${key}`);
+    };
 
     return (
         <div className="sidebar">
             <Menu
-                defaultSelectedKeys={['sub1']}
+                defaultSelectedKeys={['post']}
                 defaultOpenKeys={['1']}
+                selectedKeys={history.location.pathname}
                 mode="inline"
                 theme="dark"
             >
                 <SubMenu key="1" icon={<CommentOutlined />} title="论坛管理">
-                    <Menu.Item key="sub1"></Menu.Item>
-                    <Menu.Item key="sub2">评论管理</Menu.Item>
+                    <Menu.Item key="/post" onClick={handleItemClick}>
+                        帖子管理
+                    </Menu.Item>
+                    <Menu.Item key="/comment" onClick={handleItemClick}>
+                        评论管理
+                    </Menu.Item>
                 </SubMenu>
-                <Menu.Item key="2" icon={<CarryOutOutlined />}>
+                <Menu.Item key="/task" icon={<CarryOutOutlined />} onClick={handleItemClick}>
                     任务管理
                 </Menu.Item>
-                <Menu.Item key="3" icon={<UserOutlined />}>
+                <Menu.Item key="3" icon={<UserOutlined />} onClick={handleItemClick}>
                     用户管理
                 </Menu.Item>
-                <Menu.Item key="4" icon={<LogoutOutlined />}>
+                <Menu.Item key="/login" icon={<LogoutOutlined />} onClick={handleItemClick}>
                     退出登陆
                 </Menu.Item>
             </Menu>
